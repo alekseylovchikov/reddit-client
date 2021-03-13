f199784eee7fbecf1514ae277e5f2a02fbfad924
Aleksey Lovchikov <svdmusic@gmail.com>
2021-03-13 08:42:00 +0300

    upload images

--- src/pages/r/[sub].tsx
+++ src/pages/r/[sub].tsx
@@ -1,15 +1,32 @@
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
-import PostCard from '../../components/PostCard';
+import Head from 'next/head';
+import Image from 'next/image';
+import { ChangeEvent, createRef, useEffect, useState } from 'react';
+import classNames from 'classnames';
 
-export default function Sub() {
+import PostCard from '../../components/PostCard';
+import { Sub } from '../../types';
+
+import { useAuthState } from '../../context/auth';
+import axios from 'axios';
+
+export default function SubPage() {
+  const [ownSub, setOwnSub] = useState(false);
+  const { authenticated, user } = useAuthState();
   const router = useRouter();
+  const fileInputRef = createRef<HTMLInputElement>();
 
   const subName = router.query.sub;
 
-  const { data: sub, error } = useSWR(
+  const { data: sub, error, revalidate } = useSWR<Sub>(
     subName ? `/subs/${subName}` : null
   );
+
+  useEffect(() => {
+    if (!sub) return;
+    setOwnSub(authenticated && user.username === sub.username);
+  }, [sub]);
 
   if (error) {
     router.push('/');
@@ -29,9 +46,98 @@ export default function Sub() {
     ));
   }
 
+  function openFileInput(type: string) {
+    console.log('open');
+    if (!ownSub) return;
+    fileInputRef.current.name = type;
+    fileInputRef.current.click();
+  }
+
+  async function uploadImage(e: ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files[0];
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('type', fileInputRef.current.name);
+
+    try {
+      await axios.post<Sub>(`/subs/${sub.name}/image`, formData, {
+        headers: { 'Content-Type': 'multipart/form' },
+      });
+      revalidate();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <div className="container flex pt-5">
-      {sub && <div className="w-160">{postsMarkup}</div>}
+    <div>
+      <Head>
+        <title>{sub?.title}</title>
+      </Head>
+      {sub && (
+        <>
+          <input
+            type="file"
+            onChange={uploadImage}
+            hidden
+            ref={fileInputRef}
+          />
+          {/*sub info and images*/}
+          <div>
+            <div
+              className={classNames('bg-blue-500', {
+                'cursor-pointer': ownSub,
+              })}
+              onClick={() => openFileInput('banner')}
+            >
+              {sub.bannerUrl ? (
+                <div
+                  className="h-56 bg-blue-500"
+                  style={{
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundImage: `url(${sub.bannerUrl})`,
+                  }}
+                />
+              ) : (
+                <div className="h-20 bg-blue-500" />
+              )}
+            </div>
+            <div className="h-20 bg-white">
+              <div className="container flex relative">
+                <div className="absolute" style={{ top: -15 }}>
+                  <Image
+                    src={sub.imageUrl}
+                    alt="Sub"
+                    className={classNames('rounded-full', {
+                      'cursor-pointer': ownSub,
+                    })}
+                    onClick={() => openFileInput('image')}
+                    width={70}
+                    height={70}
+                  />
+                </div>
+                <div className="pt-1 pl-24">
+                  <div className="flex items-center">
+                    <h1 className="mb-1 text-3xl font-bold">
+                      {sub.title}
+                    </h1>
+                  </div>
+                  <p className="text-sm font-bold text-gray-500">
+                    /r/{sub.name}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*posts & sidebar*/}
+          <div className="container flex pt-5">
+            <div className="w-160">{postsMarkup}</div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
